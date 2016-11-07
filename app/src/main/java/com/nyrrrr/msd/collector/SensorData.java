589bba7885b4e4773bd8e4a96d7e9aeb0ab56a33
nyrrrr <info@nyrrrr.com>
2016-11-08 00:59:49 +0100

    update on readability of KEYCODES

--- app/src/main/java/com/nyrrrr/msd/collector/SensorData.java
+++ app/src/main/java/com/nyrrrr/msd/collector/SensorData.java
@@ -3,6 +3,7 @@ package com.nyrrrr.msd.collector;
 import android.hardware.SensorEvent;
 import android.os.Build;
 import android.util.Log;
+import android.view.KeyEvent;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,30 +23,6 @@ public class SensorData {
     private float fFrequency = -1;
     private int iOrientation = -1;
     private String sKeyPressed = "";
-
-    /**
-     * Convert Data to JSON
-     * @return
-     */
-    public JSONObject toJSONObject () {
-        JSONObject jsonObject = new JSONObject();
-        JSONObject valuesObject = new JSONObject();
-        try {
-            jsonObject.put("Timestamp", dTimestamp);
-            jsonObject.put("Sensor", sSensorType);
-            jsonObject.put("Accuracy", iAccuracy);
-            jsonObject.put("Orientation", iOrientation);
-            jsonObject.put("Key", sKeyPressed);
-
-            valuesObject.put("x", fValues[0]);
-            valuesObject.put("y", fValues[1]);
-            valuesObject.put("z", fValues[2]);
-            jsonObject.put("Values", valuesObject);
-        } catch (JSONException e) {
-            Log.e(e.getCause().toString(), "Error while converting list to JSON: " + e.getMessage());
-        }
-        return jsonObject;
-    }
 
     /**
      * @param pEvent
@@ -71,17 +48,43 @@ public class SensorData {
         iAccuracy = pEvent.accuracy;
         fFrequency = pFrequency;
         iOrientation = pOrientation;
-        sKeyPressed = (pKeyPressedCode == "KEYCODE_UNKNOWN" ? "" : pKeyPressedCode);
+        sKeyPressed = (KeyEvent.keyCodeToString(KeyEvent.KEYCODE_UNKNOWN).replaceAll("\\s", "").equals(pKeyPressedCode) ? "" : pKeyPressedCode);
+    }
+
+    /**
+     * Convert List to JSONObject
+     *
+     * @return JSONObject
+     * @throws JSONException
+     */
+    public JSONObject toJSONObject() {
+        JSONObject jsonObject = new JSONObject();
+        JSONObject valuesObject = new JSONObject();
+        try {
+            jsonObject.put("Timestamp", dTimestamp);
+            jsonObject.put("Sensor", sSensorType);
+            jsonObject.put("Accuracy", iAccuracy);
+            jsonObject.put("Orientation", iOrientation);
+            jsonObject.put("Key", sKeyPressed);
+
+            valuesObject.put("x", fValues[0]);
+            valuesObject.put("y", fValues[1]);
+            valuesObject.put("z", fValues[2]);
+            jsonObject.put("Values", valuesObject);
+        } catch (JSONException e) {
+            Log.e(e.getCause().toString(), "Error while converting list to JSON: " + e.getMessage());
+        }
+        return jsonObject;
     }
 
     public void print() {
         Log.d("Timestamp", dTimestamp + "");
-        Log.d("Sensor type", sSensorType);
+        //Log.d("Sensor type", sSensorType);
         Log.d("Values", "x: " + fValues[0] + ", y: " + fValues[1] + ", z: " + fValues[2] + "");
-        Log.d("Accuracy", iAccuracy + "");
+        //Log.d("Accuracy", iAccuracy + "");
         Log.d("Key Pressed", sKeyPressed);
         Log.d("Orientation", iOrientation + "");
-        Log.d("Frequency", fFrequency + "");
+        //Log.d("Frequency", fFrequency + "");
         Log.d("-----------------------", "-------------------------------------------------------");
     }
 }
